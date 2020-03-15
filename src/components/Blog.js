d913e7e8d762bf5d841bd26ef60aafb32c3deec6
Taylor Wilsdon <tbarrettwilsdon@gmail.com>
2020-03-15 18:43:52 -0400

    Update blog post title

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -4,7 +4,7 @@ import avatar from '../images/avatar.png';
 const Blog = (props) => (
     <section id="blogMain">
         <div id="blogBody">
-            <h2><a href="https://medium.com/@tbarrettwilsdon/multi-gig-for-the-masses-building-an-inexpensive-10gbe-network-for-the-home-cfe91740f186">Multi-Gig for the Masses</a></h2>
+            <h2><a href="https://medium.com/@tbarrettwilsdon/multi-gig-for-the-masses-building-an-inexpensive-10gbe-network-for-the-home-cfe91740f186">Multi-Gig for the Masses — Building an inexpensive 10GbE network for the home</a></h2>
             <h3>Taylor Wilsdon | 3/6/20</h3>
             <span>
                 
