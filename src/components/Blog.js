7af375148be0cdb649a1f038a5ab45566e7e5e19
Taylor Wilsdon <tbarrettwilsdon@gmail.com>
2020-03-15 18:33:14 -0400

    Add blog component

---
+++ src/components/Blog.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import avatar from '../images/avatar.png';
+
+const Blog = (props) => (
+    <section id="blogMain">
+        <div id="blogBody">
+            <h2><a href="https://medium.com/@tbarrettwilsdon/multi-gig-for-the-masses-building-an-inexpensive-10gbe-network-for-the-home-cfe91740f186">Multi-Gig for the Masses</a></h2>
+            <h3>Taylor Wilsdon | 3/6/20</h3>
+            <span>
+                
+            </span>
+        </div>
+    </section>
+)
+
+export default Blog
