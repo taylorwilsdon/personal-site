3153271e9448f5bf83bb863e495ff560da31a555
Taylor Wilsdon <taylor@taylorwilsdon.com>
2022-02-19 12:34:25 -0500

    Migrate main view to standalone component

---
+++ src/components/Main.js
@@ -0,0 +1,11 @@
+import Header from './Header';
+import Icons from './Icons';
+
+const Main = (props) => (
+    <section id="main">
+        <Header />
+        <Icons />
+    </section>
+)
+
+export default Main;
