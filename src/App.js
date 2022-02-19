0de3d1788e7bea1e2f75ee6bef55b3932fab85bd
Taylor Wilsdon <taylor@taylorwilsdon.com>
2022-02-19 17:14:19 -0500

    Migrate base app from class based to functional component, add mui library & implement card grid, add additional publications, update header nav

--- src/App.js
+++ src/App.js
@@ -1,39 +1,35 @@
-import React, { Component } from 'react';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link,
-} from "react-router-dom";
-import Blog from './components/Blog';
-import Main from './components/Main';
-import './assets/css/main.css';
+import React from "react";
+import { BrowserRouter as Router, Link, Routes, Route } from "react-router-dom";
+import Blog from "./components/Blog";
+import Main from "./components/Main";
+import "./assets/css/main.css";
 
-
-class App extends Component {
-  render() {
-    return (
-      <Router basename="/">
-        <div className="App">
-          <div id="wrapper">
-            <header>
-              <span className="navBar"><Link to="/">Home</Link> | <Link to="/blog">Blog</Link></span>
-            </header>
-            <Routes>
-              <Route path="/" element={< Main />} />
-              <Route path="/blog" element={<Blog />} />
-            </Routes>
-            <footer id="footer">
-              <ul className="copyright">
-                <li>&copy; Taylor Wilsdon</li>
-              </ul>
-            </footer>
-          </div>
+const App = (props) => {
+  const copyrightYear = new Date().getFullYear();
+  return (
+    <Router basename="/">
+      <div className="App">
+        <div id="wrapper">
+          <header>
+            <span className="navBar">
+              <Link to="/">Home</Link> | <Link to="/blog">Publications</Link> |{" "}
+              <a href="https://instagram.com/taylorwilsdon">Photography</a>
+            </span>
+          </header>
+          <br />
+          <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="/blog" element={<Blog />} />
+          </Routes>
+          <footer id="footer">
+            <ul className="copyright">
+              <li>&copy; {copyrightYear} Taylor Wilsdon</li>
+            </ul>
+          </footer>
         </div>
-      </Router>
-
-    );
-  }
-}
+      </div>
+    </Router>
+  );
+};
 
 export default App;
--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,15 +1,95 @@
-import React from 'react';
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid";
+
+import { CardActionArea } from "@mui/material";
 
 const Blog = (props) => (
-    <section id="blogMain">
-        <div id="blogBody">
-            <h2><a href="https://medium.com/@tbarrettwilsdon/multi-gig-for-the-masses-building-an-inexpensive-10gbe-network-for-the-home-cfe91740f186">Multi-Gig for the Masses — Building an inexpensive 10GbE network for the home</a></h2>
-            <h3>Taylor Wilsdon | 3/6/20</h3>
-            <span>
-                
-            </span>
-        </div>
-    </section>
-)
+    <div>
+    <Grid container align='center' spacing={1} direction='row' justify='flex-start' alignItems='flex-start'>
+      <Grid item xs={4}>
+        <Card sx={{ minWidth: 0, maxWidth: 400, minHeight: 425 }}>
+          <CardActionArea>
+          <a href="https://medium.com/@tbarrettwilsdon/multi-gig-for-the-masses-building-an-inexpensive-10gbe-network-for-the-home-cfe91740f186">
+            <CardMedia
+              component="img"
+              height="250"
+              image={require('../images/medium_card.png')}
+              alt="Multi-Gig for the Masses"
+            />
+            <CardContent>
+              <Typography variant="h5" component="div">
+              Multi-Gig for the Masses
+              </Typography>
+              <Typography variant="h7" component="div">
+              Building an inexpensive 10GbE network for the home
+              </Typography>
+              <Typography variant="body2" style={{whiteSpace:'pre-wrap'}} color="text.secondary">
+                Thoughts on design and component selection for a multi-gigabit LAN (2.5/5/10GbE) based on my experiences building my home network architecture.
+              </Typography>
+            </CardContent>
+            </a>
+          </CardActionArea>
+        </Card>
+      </Grid>
+      <Grid item xs={4}>
+        <Card sx={{ minWidth: 0, maxWidth: 400, minHeight: 425 }}>
+        <a href="https://www.conferencecast.tv/speaker-32221-taylor-barrett-wilsdon#.speakerPage-latest">
+          <CardActionArea>
+            <CardMedia
+              component="img"
+              height="250"
+              image={require('../images/summit_card.png')}
+              alt="Atlassian Summit"
+            />
+            <CardContent>
+              <Typography variant="h5" component="div">
+                Atlassian Summit | Jira & Ansible
+              </Typography>
+              <Typography variant="h7" component="div">
+              Scaling Jira Server Administration for the Enterprise
+              </Typography>
+              <Typography variant="body2" style={{whiteSpace:'pre-wrap'}} color="text.secondary">
+            Speaking on behalf of Yelp at Atlassain Summit 2019<br /> in Las Vegas, covering the technical architecture<br/>that allowed
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+          </a>
+        </Card>
+      </Grid>
+      <Grid item xs={4}>
+        <Card sx={{ minWidth: 0, maxWidth: 400, minHeight: 425 }}>
+          <CardActionArea>
+              <a href="https://www.nytimes.com/interactive/2022/02/17/realestate/17hunt-wilsdon.html?unlocked_article_code=AAAAAAAAAAAAAAAACEIPuonUyYiZ_tU1Gw5CRWySB4B99Fre37-Wn_smi3b6ITOJQCpPiL1KA5WG5wLSZK54a9w1mTSHQdsLeJkeeMtP9M4NdUp8V1vv5ZqChJkfKi4pqJy-GTJvh5nDAugztGa0NDf8deojnPi17hiLdWHoDaXd1nxwIwxio4B3Ng3_gQx-zajOG-9w3NB5zK1hUsg8HWFcEXHM6_r4CBx-O8GEbQXf7GQ1XOJbWzLSmL2M-u5KMVUSWR-dEiQJsStr48hcOdgXIK_-MxchHcL6irwQAWZvR5y7nQlhdunCsD11g1VV9a4C5Q&referringSource=articleShare">
+            <CardMedia
+              component="img"
+              height="250"
+              image={require('../images/nyt_card.png')}
+              alt="NYT - The Hunt"
+            />
+            <CardContent>
+              <Typography variant="h5" component="div">
+                The New York Times
+              </Typography>
+              <Typography variant="h7" component="div">
+              Real Estate | The Hunt
+              </Typography>
+              <Typography variant="body2" style={{whiteSpace:'pre-wrap'}} color="text.secondary">
+                Interview with Joyce Cohen of the New York Times for The Hunt,
+                focused on our experience finding our New York City
+                home.
+              </Typography>
+            </CardContent>
+            </a>
+          </CardActionArea>
+        </Card>
+      </Grid>
 
-export default Blog
+    </Grid>
+    </div>
+);
+
+export default Blog;
