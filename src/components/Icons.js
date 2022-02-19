01159442f6b37514374794d05111301d11169e49
Taylor Wilsdon <taylor@taylorwilsdon.com>
2022-02-19 17:21:56 -0500

    Update github footer link

--- src/components/Icons.js
+++ src/components/Icons.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Icons = (props) => (
     <footer>
     <ul className="icons">
-      <li><a href="https://github.com/tbarrettwilsdon" className="fa-github">Github</a></li>
+      <li><a href="https://github.com/taylorwilsdon" className="fa-github">Github</a></li>
       <li><a href="https://www.instagram.com/taylorwilsdon/" className="fa-instagram">Instagram</a></li>
       <li><a href="https://www.linkedin.com/in/taylorbarrettwilsdon/" className="fa-linkedin">LinkedIn</a></li>
     </ul>
