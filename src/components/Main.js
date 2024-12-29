ec1afacacba2a7c9a31c4668b9a6027d932f98bb
Taylor Wilsdon <taylor@taylorwilsdon.com>
2024-12-28 19:02:09 -0500

    Add icon svgs

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,15 @@
 import Header from './Header';
 import Icons from './Icons';
+import GitHubActivityLog from './Github';
 
 const Main = (props) => (
+    <div>
     <section id="main">
         <Header />
+        <GitHubActivityLog username="taylorwilsdon" />
         <Icons />
     </section>
+    </div>
 )
 
 export default Main;
