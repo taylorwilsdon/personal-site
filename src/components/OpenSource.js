09d851eb1c3236af915ab0202cac0ebf93631c97
Taylor Wilsdon <taylor@taylorwilsdon.com>
2024-12-28 22:13:12 -0500

    Add open source view

--- src/components/OpenSource.js
+++ src/components/OpenSource.js
@@ -1,18 +1,28 @@
-import { Grid, Card, CardActionArea, CardMedia, CardContent, Typography } from '@mui/material';
+import { Grid, Card, CardActionArea, CardContent, Typography, createTheme } from '@mui/material';
+
+const theme = createTheme();
+
+theme.typography.body2 = {
+  fontSize: '0.675rem',
+  '@media (min-width:600px)': {
+    fontSize: '.675rem',
+  },
+};
+
 
 const OpenSource = () => (
   <div>
-    <h3>Open Source Contributions</h3>
+    <h3>projects I believe in, support & contribute to</h3>
     <Grid container spacing={2} justifyContent="center">
       <Grid item xs={12} md={6}>
         <Card>
-          <CardActionArea href="https://github.com/taylorwilsdon" target="_blank" rel="noopener noreferrer">
+          <CardActionArea href="https://github.com/netbirdio/netbird " target="_blank" rel="noopener noreferrer">
             <CardContent>
               <Typography variant="h5" component="div">
-                GitHub Profile
+                netbird
               </Typography>
               <Typography variant="body2" color="text.secondary">
-                Explore my open source projects and contributions on GitHub
+                wireguard overlay networking
               </Typography>
             </CardContent>
           </CardActionArea>
@@ -20,18 +30,47 @@ const OpenSource = () => (
       </Grid>
       <Grid item xs={12} md={6}>
         <Card>
-          <CardActionArea href="https://github.com/Yelp/detect-secrets" target="_blank" rel="noopener noreferrer">
+          <CardActionArea href="https://github.com/homebridge/homebridge" target="_blank" rel="noopener noreferrer">
             <CardContent>
               <Typography variant="h5" component="div">
-                detect-secrets
+                Homebridge
               </Typography>
               <Typography variant="body2" color="text.secondary">
-                Contributed to Yelp's secret detection tool
+              HomeKit support for the impatient
               </Typography>
             </CardContent>
           </CardActionArea>
         </Card>
       </Grid>
+      <Grid item xs={12} md={6}>
+        <Card>
+          <CardActionArea href="https://github.com/open-webui/open-webui" target="_blank" rel="noopener noreferrer">
+            <CardContent>
+              <Typography variant="h5" component="div">
+                open-webui
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+              the gold standard for llm chat ui
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      </Grid>
+      <Grid item xs={12} md={6}>
+        <Card>
+          <CardActionArea href="https://github.com/ollama/ollama" target="_blank" rel="noopener noreferrer">
+            <CardContent>
+              <Typography variant="h5" component="div">
+                ollama
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+              llama.cpp wrapper for dead simple llm inference 
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      </Grid>
+
     </Grid>
   </div>
 );
