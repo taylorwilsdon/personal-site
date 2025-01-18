ef049a3466cf73e2caf8177f947af24e1b99d70e
Taylor Wilsdon <taylor@taylorwilsdon.com>
2025-01-18 13:55:08 -0500

    refactor: centralize articles data and create reusable ArticleCard component

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -10,9 +10,49 @@ import {
 import * as React from "react";
 import { useState, useEffect } from "react";
 
-
-// import MarkdownRenderer from './MarkdownRenderer';
-
+// Centralized articles data
+const articles = [
+  {
+    href: "https://medium.com/@tbarrettwilsdon/multi-gig-for-the-masses-building-an-inexpensive-10gbe-network-for-the-home-cfe91740f186",
+    image: require('../images/medium_card.png'),
+    alt: "Multi-Gig for the Masses",
+    title: "Multi-Gig for the Masses",
+    subtitle: "Building an inexpensive 10GbE network",
+    description: "Thoughts on design and component selection for a multi-gigabit LAN (2.5/5/10GbE) based on my experiences building my home network architecture"
+  },
+  {
+    href: "https://www.conferencecast.tv/speaker-32221-taylor-barrett-wilsdon#.speakerPage-latest",
+    image: require('../images/summit_card.png'),
+    alt: "Atlassian Summit",
+    title: "Scaling Jira Server Administration for the Enterprise",
+    subtitle: "Atlassian Summit 2019 - Jira & Ansible",
+    description: "Speaking on behalf of Yelp at Atlassian Summit 2019 in Las Vegas, covering the technical architecture that allowed Yelp to run Jira at scale"
+  },
+  {
+    href: "https://medium.com/@tbarrettwilsdon/smart-zoned-nyc-steam-radiators-effectively-controlling-one-pipe-steam-heat-in-apartments-304a2ed2cd1b",
+    image: require('../images/vent.png'),
+    alt: "Smart Steam Heat",
+    title: "Smart Zoned Steam Radiators",
+    subtitle: "Modernizing control of one pipe steam heat",
+    description: "A look into the misadventures and solutions as I implemented smart, individual zone control for my NYC apartment's cast iron one pipe steam radiators"
+  },
+  {
+    href: "https://taylorwilsdon.medium.com/in-the-pursuit-of-a-more-refined-window-ac-installation-building-in-midea-u-shape-inverter-splits-9bd3b83b0351",
+    image: require('../images/ac.jpg'),
+    alt: "Air Conditioner",
+    title: "The pursuit of a more refined window AC installation",
+    subtitle: "Building in Midea U-Shape Inverter Splits",
+    description: "Building, insulating and finishing the closest thing to a true mini split for a clean, silent installation"
+  },
+  {
+    href: "https://www.nytimes.com/interactive/2022/02/17/realestate/17hunt-wilsdon.html",
+    image: require('../images/nyt_card.png'),
+    alt: "NYT - The Hunt",
+    title: "The New York Times",
+    subtitle: "Real Estate | The Hunt",
+    description: "Interview with Joyce Cohen of the New York Times for The Hunt, focused on our experience finding our New York City home"
+  }
+];
 
 // Styled components
 const StyledGrid = styled(Grid)(({ theme }) => ({
@@ -35,13 +75,6 @@ const StyledCardMedia = styled(CardMedia)({
   height: 175,
   width: 320,
 });
-
-// const StyledMarkdownPaper = styled(Paper)(({ theme }) => ({
-//   marginTop: theme.spacing(4),
-//   padding: theme.spacing(3),
-//   backgroundColor: '#fff',
-//   boxShadow: '0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)',
-// }));
 
 const StyledLink = styled('a')({
   textDecoration: 'none',
@@ -73,6 +106,32 @@ const StyledDescription = styled(Typography)({
   lineHeight: '1.4'
 });
 
+// ArticleCard component
+const ArticleCard = ({ article }) => (
+  <StyledCard>
+    <CardActionArea>
+      <StyledLink href={article.href}>
+        <StyledCardMedia
+          component="img"
+          image={article.image}
+          alt={article.alt}
+        />
+        <CardContent>
+          <StyledCardTitle>
+            {article.title}
+          </StyledCardTitle>
+          <StyledSubtitle variant="subtitle2">
+            {article.subtitle}
+          </StyledSubtitle>
+          <StyledDescription>
+            {article.description}
+          </StyledDescription>
+        </CardContent>
+      </StyledLink>
+    </CardActionArea>
+  </StyledCard>
+);
+
 const Blog = (props) => {
   const [markdown, setMarkdown] = useState('');
 
@@ -93,139 +152,11 @@ const Blog = (props) => {
   return (
     <div>
       <StyledGrid container spacing={2}>
-        <Grid item xs={4}>
-          <StyledCard>
-            <CardActionArea>
-              <StyledLink href="https://medium.com/@tbarrettwilsdon/multi-gig-for-the-masses-building-an-inexpensive-10gbe-network-for-the-home-cfe91740f186">
-                <StyledCardMedia
-                  component="img"
-                  image={require('../images/medium_card.png')}
-                  alt="Multi-Gig for the Masses"
-                />
-                <CardContent>
-                  <StyledCardTitle>
-                    Multi-Gig for the Masses
-                  </StyledCardTitle>
-                  <StyledSubtitle variant="subtitle2">
-                    Building an inexpensive 10GbE network
-                  </StyledSubtitle>
-                  <StyledDescription>
-                    Thoughts on design and component selection for a multi-gigabit LAN (2.5/5/10GbE) based on my experiences building my home network architecture
-                  </StyledDescription>
-                </CardContent>
-              </StyledLink>
-            </CardActionArea>
-          </StyledCard>
-        </Grid>
-
-        <Grid item xs={4}>
-          <StyledCard>
-            <CardActionArea>
-              <StyledLink href="https://www.conferencecast.tv/speaker-32221-taylor-barrett-wilsdon#.speakerPage-latest">
-                <StyledCardMedia
-                  component="img"
-                  image={require('../images/summit_card.png')}
-                  alt="Atlassian Summit"
-                />
-                <CardContent>
-                <StyledCardTitle>
-                Scaling Jira Server Administration<br/> for the Enterprise
-                  </StyledCardTitle>
-                  <Typography variant="subtitle2" component="div">
-                  Atlassian Summit 2019 - Jira & Ansible
-                  </Typography>
-                  <StyledDescription>
-                  Speaking on behalf of Yelp at Atlassain Summit 2019 in Las Vegas, covering the technical architecture that allowed Yelp to run Jira at scale
-                  </StyledDescription>
-                </CardContent>
-              </StyledLink>
-            </CardActionArea>
-          </StyledCard>
-        </Grid>
-
-        <Grid item xs={4}>
-          <StyledCard>
-            <CardActionArea>
-              <StyledLink href="https://medium.com/@tbarrettwilsdon/smart-zoned-nyc-steam-radiators-effectively-controlling-one-pipe-steam-heat-in-apartments-304a2ed2cd1b">
-                <StyledCardMedia
-                  component="img"
-                  image={require('../images/vent.png')}
-                  alt="Smart Steam Heat"
-                />
-                <CardContent>
-                  <StyledCardTitle>
-                    Smart Zoned Steam Radiators
-                  </StyledCardTitle>
-                  <StyledSubtitle variant="subtitle2">
-                    Modernizing control of one pipe steam heat
-                  </StyledSubtitle>
-                  <StyledDescription>
-                    A look into the misadventures and solutions as I implemented smart, individual zone control for my NYC apartment's cast iron one pipe steam radiators
-                  </StyledDescription>
-                </CardContent>
-              </StyledLink>
-            </CardActionArea>
-          </StyledCard>
-        </Grid>
-
-        <Grid item xs={4}>
-          <StyledCard>
-            <CardActionArea>
-              <StyledLink href="https://taylorwilsdon.medium.com/in-the-pursuit-of-a-more-refined-window-ac-installation-building-in-midea-u-shape-inverter-splits-9bd3b83b0351">
-                <StyledCardMedia
-                  component="img"
-                  image={require('../images/ac.jpg')}
-                  alt="Air Conditioner"
-                />
-                <CardContent>
-                  <StyledCardTitle>
-                  The pursuit of a more refined <br />
-                  window AC installation
-                  </StyledCardTitle>
-                  <StyledSubtitle variant="subtitle2">
-                  Building in Midea U-Shape Inverter Splits
-                  </StyledSubtitle>
-                  <StyledDescription>
-Building, insulating and finishing the closest thing to a true mini split for a clean, silent installation
-                  </StyledDescription>
-                </CardContent>
-              </StyledLink>
-            </CardActionArea>
-          </StyledCard>
-        </Grid>
-
-        <Grid item xs={4}>
-          <StyledCard>
-            <CardActionArea>
-              <StyledLink href="https://www.nytimes.com/interactive/2022/02/17/realestate/17hunt-wilsdon.html">
-                <StyledCardMedia
-                  component="img"
-                  image={require('../images/nyt_card.png')}
-                  alt="NYT - The Hunt"
-                />
-                <CardContent>
-                <StyledCardTitle>
-                    The New York Times
-                    </StyledCardTitle>
-                    <Typography variant="subtitle2" component="div">
-                    Real Estate | The Hunt
-                  </Typography>
-                  <StyledDescription>
-                    Interview with Joyce Cohen of the New York Times for The Hunt,
-                    focused on our experience finding our New York City
-                    home
-                  </StyledDescription>
-                </CardContent>
-              </StyledLink>
-            </CardActionArea>
-          </StyledCard>
-        </Grid>
-
-        {/* <Grid item xs={12}>
-          <StyledMarkdownPaper elevation={1}>
-            <MarkdownRenderer content={markdown} />
-          </StyledMarkdownPaper>
-        </Grid> */}
+        {articles.map((article, index) => (
+          <Grid item xs={4} key={index}>
+            <ArticleCard article={article} />
+          </Grid>
+        ))}
       </StyledGrid>
     </div>
   );
