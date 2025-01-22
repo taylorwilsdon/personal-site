d8db81d6e23870e66d976df061b6dda7e74289bb
Taylor Wilsdon <taylor@taylorwilsdon.com>
2025-01-22 17:56:35 -0500

    style: make Main component responsive for mobile while preserving desktop layout

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,17 +2,36 @@ import GitHubActivityLog from './Github';
 import Header from './Header';
 import Icons from './Icons';
 import Photography from './Photography';
+import { styled } from '@mui/system';
+
+const MainContainer = styled('div')(({ theme }) => ({
+  maxWidth: '100%',
+  overflowX: 'hidden',
+  [theme.breakpoints.down('sm')]: {
+    padding: '0 16px',
+  },
+}));
+
+const MainSection = styled('section')(({ theme }) => ({
+  maxWidth: '1200px',
+  margin: '0 auto',
+  [theme.breakpoints.down('sm')]: {
+    maxWidth: '100%',
+    padding: '0 8px',
+  },
+}));
+
 const Main = (props) => (
-    <div>
-    <section id="main">
-        <Header />
-        <GitHubActivityLog username="taylorwilsdon" />
-        <br />
-        <Photography />
-        <br />
-        <Icons />
-    </section>
-    </div>
+  <MainContainer>
+    <MainSection id="main">
+      <Header />
+      <GitHubActivityLog username="taylorwilsdon" />
+      <br />
+      <Photography />
+      <br />
+      <Icons />
+    </MainSection>
+  </MainContainer>
 )
 
 export default Main;
