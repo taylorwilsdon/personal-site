778155b63f98003c709d5d90c72a09041a3a7000
Taylor Wilsdon <taylor@taylorwilsdon.com>
2025-01-22 18:00:05 -0500

    style: add responsive styling to OpenSourcePage component

--- src/components/OpenSourcePage.js
+++ src/components/OpenSourcePage.js
@@ -1,14 +1,36 @@
+import { styled } from '@mui/system';
 import Icons from './Icons';
 import OpenSource from './OpenSource';
 
+const MainContainer = styled('div')(({ theme }) => ({
+  maxWidth: '100vw',
+  overflowX: 'hidden',
+  [theme.breakpoints.down('sm')]: {
+    padding: '0 16px',
+    width: '100vw',
+    boxSizing: 'border-box',
+  },
+}));
+
+const MainSection = styled('section')(({ theme }) => ({
+  maxWidth: '1200px',
+  margin: '0 auto',
+  [theme.breakpoints.down('sm')]: {
+    maxWidth: '100%',
+    width: '100%',
+    padding: '0 8px',
+    boxSizing: 'border-box',
+  },
+}));
+
 const OpenSourcePage = () => (
-    <div>
-    <section id="main">
-        <OpenSource />
-        <br />
-        <Icons />
-    </section>
-    </div>
+  <MainContainer>
+    <MainSection id="main">
+      <OpenSource />
+      <br />
+      <Icons />
+    </MainSection>
+  </MainContainer>
 );
 
 export default OpenSourcePage;
